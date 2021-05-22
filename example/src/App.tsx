d14677cacf727808b5dced348d74e519640954ea
Nguyen Phat <phatnt.uit@gmail.com>
2021-05-22 21:58:19 +0700

    refactor: refactor dependencies

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,27 +1,10 @@
-import { SayHello } from 'react-components';
-import logo from './logo.svg';
+import { SayHello } from '@phatnguyenuit/react-components';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div>
-        <SayHello name="Phat" />
-      </div>
+      <SayHello name="Phat" />
     </div>
   );
 }
--- src/components/say-hello/index.tsx
+++ src/components/say-hello/index.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const SayHello: React.FC<SayHelloProps> = ({ name }) => (
-  <div>Hey {name}, say hello to TypeScript.</div>
-);
+const SayHello: React.FC<SayHelloProps> = ({ name }) => {
+  const [count, setCount] = useState(3);
+  const [intervalId, setIntervalId] = useState<number>();
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCount((prev) => prev - 1);
+    }, 1000);
+    setIntervalId(id);
+    return () => {
+      clearInterval(id);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (count === 0) {
+      setIntervalId(undefined);
+      clearInterval(intervalId);
+    }
+  }, [count]);
+
+  const message = count === 0 ? `Hey ${name}, say hello to TypeScript.` : count;
+
+  return <div>{message}</div>;
+};
 
 export default SayHello;
 
